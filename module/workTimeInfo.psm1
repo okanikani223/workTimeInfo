5f9e55a69816c7661d62481807147f1d30714f99
okanikani223 <k.hidaka63@gmail.com>
2017-02-03 18:41:16 +0900

    add code comment

--- module/workTimeInfo.psm1
+++ module/workTimeInfo.psm1
@@ -1,8 +1,19 @@
 $adjustmentMinuts = [System.TimeSpan]::FromSeconds(449);
 $restTime         = [System.TimeSpan]::FromHours(1);
+# 時間を15分単位で均す関数
 function makeEvenTime ([System.TimeSpan]$time) {
     [System.TimeSpan]::FromMinutes([Math]::Truncate($time.Add($adjustmentMinuts).TotalMinutes / 15) * 15);
 };
+# イベントログから指定された範囲の期間で作業時間情報を取得する関数
+# 取得する作業情報は以下の通り
+# 　１．日毎の起動、終了、稼働時間
+# 　２．平日の総稼働日数
+# 　３．平日の総稼働時間
+# 　４．休日の総稼働日数
+# 　５．休日の総稼働時間
+# 　６．平日と休日の総稼働日数の合計
+# 　７．平日と休日の総稼働時間の合計
+# isEven フラグで、時間の均し処理(15分単位)の有無を変更可能
 function workTimeInfo ($start, $end, $isEven) {
     $workingDays = (Get-EventLog system -After $start -Before $end) + (Get-EventLog application -After $start -Before $end) |
     group{$_.TimeWritten.ToShortDateString()} |
